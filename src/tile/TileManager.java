7e134964ce98d04d5d4bce8a36cbe6c4ff6f4111
elepoprojek <elepoprojek@gmail.com>
2025-12-29 16:00:32 +0700

    hello

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -22,48 +22,80 @@ public class TileManager {
 		this.gp = gp;
 		mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
 		
-		tile = new Tile[50];
+		tile = new Tile[70];
 		
 		getTileImage();	
-		loadMap("/maps/world01.txt");
+		loadMap("/maps/maps1.txt");
 	}
 	
 	public void getTileImage() {
 			
 			//Pembatas
-			setup(0, "jalan1", false);
-			setup(1, "jalan1", false);
-			setup(2, "jalan1", false);
-			setup(3, "jalan1", false);
-			setup(4, "jalan1", false);
-			setup(5, "jalan1", false);
-			setup(6, "jalan1", false);
-			setup(7, "jalan1", false);
-			setup(8, "jalan1", false);
-			setup(9, "jalan1", false);
-			//Pembatas
-			
-			
-			setup(10, "jalan1", false);
-			setup(11, "jalan2", false);
-			setup(12, "air1", true);
-			setup(13, "air2", true);
-			setup(14, "rumput1", false);
-			setup(15, "rumput2", false);
-			setup(16, "kolam11", true);
-			setup(17, "kolam12", true);
-			setup(18, "kolam13", true);
-			setup(19, "kolam21", true);
-			setup(20, "kolam22", true);
-			setup(21, "kolam23", true);
-			setup(22, "kolam31", true);
-			setup(23, "kolam32", true);
-			setup(24, "kolam33", true);
-			setup(25, "sea11", true);
-			setup(26, "sea12", true);
-			setup(27, "sea21", true);
-			setup(28, "sea22", true);
-				
+			setup(0, "air", true); //dipakai
+//			setup(1, "batu1", false);
+			setup(2, "sungai11", true); //dipakai
+			setup(3, "sungai01", true); //dipakai
+			setup(4, "jembatan1", false); //dipakai
+//			setup(5, "pohon1", true);
+//			setup(6, "batu6", false);
+			setup(7, "jalan01", false); //dipakai
+			setup(8, "jalan02", false);
+			setup(9, "jalan03", false);
+			setup(10, "jalan04", false);
+			setup(11, "jalan4", false); //dipakai
+			setup(12, "jalan11", false); //dipakai
+			setup(13, "jalan12", false); //dipakai
+			setup(14, "jalan13", false); //dipakai
+			setup(15, "jalan14", false); //dipakai
+			setup(16, "jalan15", false); //dipakai
+//			setup(17, "jalan7", true);
+//			setup(18, "jalan8", true);
+//			setup(19, "jalan2", true);
+//			setup(20, "jalan12", true);
+//			setup(21, "jalan13", true);
+//			setup(22, "jalan14", true);
+			setup(23, "jalan2", false); //dipakai
+			setup(24, "jalan3", false); //dipakai
+//			setup(25, "jembatan2", true);
+			setup(26, "jalan5", false); //dipakai
+			setup(27, "jalan6", false); //dipakai
+			setup(28, "jalan7", false);	//dipakai		
+			setup(29, "jalan8", false); //dipakai
+//			setup(30, "pohon4", false);
+			setup(31, "jembatan2", false); //dipakai
+			setup(32, "jembatan3", false); //dipakai
+//			setup(33, "rumah", false);
+//			setup(34, "sungai04", false);
+//			setup(35, "semak1", true);
+//			setup(36, "semak2", true);
+//			setup(37, "sungai07", true);
+//			setup(38, "sungai02", true);
+//			setup(39, "sungai03", true);
+			setup(40, "rumput", false); //dipakai
+//			setup(41, "sungai05", true);
+//			setup(42, "sungai06", true);
+			setup(43, "sungai01", false); //dipakai
+			setup(44, "sungai02", false); //dipakai
+			setup(45, "sungai03", false); //dipakai
+			setup(46, "sungai04", false); //dipakai
+			setup(47, "sungai05", false); //dipakai
+			setup(48, "sungai06", false); //dipakai
+			setup(49, "sungai07", false); //dipakai
+			setup(50, "sungai08", false); //dipakai
+//			setup(51, "sungai6", true);
+			setup(52, "sungai5", true); //dipakai
+			setup(53, "sungai10", false); //dipakai
+			setup(54, "sungai11", false); //dipakai
+			setup(55, "sungai12", false); //dipakai
+			setup(56, "sungai2", true); //dipakai
+			setup(57, "sungai8", true);  //dipakai
+			setup(58, "sungai4", true); //dipakai
+//			setup(59, "hitam3", true);
+			setup(60, "sungai6", true); //dipakai
+			setup(61, "sungai7", true); //dipakai
+			setup(62, "sungai3", true); //dipakai
+			setup(63, "tanah", true); //dipakai
+
 	}
 	
 	public void setup(int index, String imageName, boolean collision) {
